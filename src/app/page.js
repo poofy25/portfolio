d105796088eccfac3b13764be5e0696e0c8208f1
poofy25 <117314241+poofy25@users.noreply.github.com>
2024-04-06 12:09:55 +0300

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import HeroComponent from "@/components/Home/Home";
 import AboutSection from "@/components/About/About";
 import ProjectsSection from "@/components/Projects/Projects";
 import ContactSection from "@/components/Contact/Contact";
+import Footer from "@/components/Footer/Footer";
 
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
         <AboutSection/>
         <ProjectsSection/>
         <ContactSection/>
+        <Footer/>
       </main>
     </>
   );
